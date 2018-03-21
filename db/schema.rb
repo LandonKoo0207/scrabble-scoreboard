84809625c6c3fa76a9727881b934ee30dfb3dd1f
Landon Koo <landonkoo0207@gmail.com>
2018-03-21 15:03:30 +1100

    Change database settings to Postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180320112816) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "players", force: :cascade do |t|
     t.string "name"
     t.integer "turn"
