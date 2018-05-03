874445396d67dbc908841b4bdcebbfe606fd1917
Landon Koo <landonkoo0207@gmail.com>
2018-05-03 17:36:30 +1000

    Refactred: Word.rb

--- app/models/word.rb
+++ app/models/word.rb
@@ -20,7 +20,9 @@ class Word < ApplicationRecord
   end
 
   def calculate_score
-    score = {'A':1, 'B':3, 'C':3, 'D':2, 'E':1, 'F':4, 'G':2, 'H':4, 'I':1, 'J':8, 'K':5, 'L':1, 'M':3, 'N':1, 'O':1, 'P':3, 'Q':10,'R':1,'S':1,'T':1,'U':1,'V':4,'W':4,'X':8,'Y':4,'Z':10}
+    score = {'A':1, 'B':3, 'C':3, 'D':2, 'E':1, 'F':4, 'G':2, 'H':4, 'I':1, 'J':8, \
+             'K':5, 'L':1, 'M':3, 'N':1, 'O':1, 'P':3, 'Q':10,'R':1,'S':1,'T':1, \
+             'U':1,'V':4,'W':4,'X':8,'Y':4,'Z':10}
     s = 0
     self[:word].each_char.with_index do |char, i|
       if self[:double_letter].include? i+1
@@ -32,13 +34,8 @@ class Word < ApplicationRecord
       end
     end
 
-    if self[:double_word]
-      s *= 2
-    end
-
-    if self[:triple_word]
-      s *= 3
-    end
+    s *= 2 if self[:double_word]
+    s *= 3 if self[:triple_word]
 
     self[:score] = s
   end
