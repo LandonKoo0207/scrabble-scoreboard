4751c346e5aa6f10dfa333b2bbffa91afab54447
Landon Koo <landonkoo0207@gmail.com>
2018-05-22 22:29:25 +1000

    Action mailer config setting for production was added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,14 +93,14 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: 'gmail.com',
-    authentication: "login",
+    authentication: 'plain',
     enable_starttls_auto: true,
     user_name: 'landonkoo0207@gmail.com',
     password: 'Kpk47503!'
